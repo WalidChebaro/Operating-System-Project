1f029217aaba1d14bc430645d56a97c25522ccbd
WalidChebaro <55446702+WalidChebaro@users.noreply.github.com>
2019-11-14 01:04:41 +0200

    Add files via upload

--- shell_project.c
+++ shell_project.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-
+#define SIZE_OF_COMMAND_LINE 100
+#define NUMBER_OF_COMMAND_LINE 1024
 /* Selected Commands of the Shell */
 void cmd_exit();
 void cmd_help();
@@ -13,42 +14,43 @@ void cmd_ls();
 void cmd_mkdir();
 void cmd_create_file();
 void cmd_delete_file();
-char command[][50]={'\0'};
+char command[NUMBER_OF_COMMAND_LINE][SIZE_OF_COMMAND_LINE];
 
-//void cmd_test(){
-//     int i;
-//     for(i=0;i<3;i++)
-//     {
-//         printf("%s\n", command[i]);
-//     }
-// }
+void cmd_test()
+{
+    int i;
+    for (i = 0; i < NUMBER_OF_COMMAND_LINE; i++)
+    {
+        printf("%s\n", command[i]);
+    }
+}
 
-char helper_cmd_table[] = {
-    {"?"},
-    {"exit"},
-    {"pwd"},
-    {"cd"},
-    {"ls"},
-    {"mkdir"},
-    {"create_file"},
-    {"delete_file"}};
+char helper_cmd_table[8][20] = {
+    "?",
+    "exit",
+    "pwd",
+    "cd",
+    "ls",
+    "mkdir",
+    "create_file",
+    "delete_file"};
 
-char cmd_table[8][50]= 
+char cmd_table[8][50] =
     {"help - show the help menu",
-    "exit - exit the shell",
-    "pwd - print the working directory",
-    "cd - change the directory",
-    "ls - list the current directory",
-    "mkdir - create a directory",
-    "create_file - create a new file",
-    "delete_file - delete the current file"};
+     "exit - exit the shell",
+     "pwd - print the working directory",
+     "cd - change the directory",
+     "ls - list the current directory",
+     "mkdir - create a directory",
+     "create_file - create a new file",
+     "delete_file - delete the current file"};
 
 void cmd_help()
 {
     int i;
-    for (i=0;i<9;i++)
+    for (i = 0; i < 9; i++)
     {
-        printf("%s\n", cmd_table[i]);    
+        printf("%s\n", cmd_table[i]);
     }
 }
 
@@ -58,13 +60,12 @@ void cmd_exit()
 }
 
 void cmd_pwd()
-{  
+{
     execlp("pwd", "pwd", NULL);
 }
 
 void cmd_cd()
 {
-    
 }
 
 void cmd_ls()
@@ -74,111 +75,102 @@ void cmd_ls()
 
 void cmd_mkdir()
 {
-
 }
 
 void cmd_chdir()
 {
-
 }
 
 void cmd_create_file()
 {
-
 }
 
 void cmd_delete_file()
 {
+}
+void command_definer(int i)
+{
+    char *token = strtok(command[i + 1], " ");
+    if (strcmp(token, helper_cmd_table[0]) == 0)
+    {
+        cmd_help();
+    }
 
-}   
+    if (strcmp(token, helper_cmd_table[1]) == 0)
+    {
 
+        cmd_exit();
+    }
 
-void main_loop(){
-    char line;
+    if (strcmp(token, helper_cmd_table[2]) == 0)
+    {
+
+        cmd_pwd();
+    }
+
+    if (strcmp(token, helper_cmd_table[3]) == 0)
+    {
+
+        cmd_cd();
+    }
+
+    if (strcmp(token, helper_cmd_table[4]) == 0)
+    {
+
+        cmd_ls();
+    }
+
+    if (strcmp(token, helper_cmd_table[5]) == 0)
+    {
+
+        cmd_mkdir();
+    }
+
+    if (strcmp(token, helper_cmd_table[6]) == 0)
+
+    {
+        cmd_create_file();
+    }
+
+    if (strcmp(token, helper_cmd_table[7]) == 0)
+    {
+
+        cmd_delete_file();
+    }
+}
+void main_loop()
+{
     char cmd;
     int i;
 
-    for(i = 0; i < sizeof(command); i++)
+    for (i = 0; i < NUMBER_OF_COMMAND_LINE; i++)
     {
-        printf("%d: ", i);    
-        scanf("%s", &line);
 
-        if(line == '\0' || line == NULL)
-        {
-            printf("%d: ", i++);
-        }else{
-            command[i]=line;
-        }
+        printf("%d: ", i);
+        scanf("%s", &command[i][SIZE_OF_COMMAND_LINE]);
 
-        // duplicate string to use its substring
-        strcpy(cmd, line);
-
-        if (strstr(cmd, helper_cmd_table[0]))
-        {
-            cmd_help();
-        }
-
-        if (strstr(cmd, helper_cmd_table[1]))
-        {
-            cmd_exit();
-        }
-
-        if (strstr(cmd, helper_cmd_table[2]))
-        {
-            cmd_pwd();
-        }
-
-        if (strstr(cmd, helper_cmd_table[3]))
-        {
-            cmd_cd();
-        }
-
-        if (strstr(cmd, helper_cmd_table[4]))
-        {
-            cmd_ls();
-        }
-
-        if (strstr(cmd, helper_cmd_table[5]))
-        {
-            ccmd_mkdir();
-        }
-
-        if (strstr(cmd, helper_cmd_table[6]))
-        {
-            cmd_create_file();
-        }
-
-        if (strstr(cmd, helper_cmd_table[7]))
-        {
-            cmd_delete_file();
-        }
+        command_definer(i);
     }
-
-   // cmd_test();
-
 }
 
-
- 
 int main(int argc, char *argv[])
 {
     /* start a program over the one running at all time, the kernel.
     But it is a copy of the kernel, that's why we use exec to run a 
     different program*/
     pid_t pid = fork();
-    
-    if(pid<0)
+
+    if (pid < 0)
     {
         perror("ERROR");
     }
 
-    if(pid==0)
+    if (pid == 0)
     {
-       main_loop();
-       
+        main_loop();
     }
 
-    if(pid>0)
+    if (pid > 0)
     {
         wait(NULL);
     }
