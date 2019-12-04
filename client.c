50654385f0f7b9d8781cd650b011ddba6b4454a3
WalidChebaro <55446702+WalidChebaro@users.noreply.github.com>
2019-12-04 10:27:28 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -1,49 +1,101 @@
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
-#define PORT 4321
-   
-int main(int argc, char const *argv[]) 
-{ 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char line[1024];
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    while(1){
-    // char path[1024];
-    int dontRead = 1;
-    read(sock,line,1024);//get current directory from server
-    printf("CLIENT SHELL -:- %s:", line);
-    fgets(line,sizeof(line),stdin);//read from stdinof user the command
-    if(line[0] == 'c' && line[1] == 'd')dontRead = 0;//if command is cd
-    if(line[0] == '.' && line[1] == '/')dontRead = 0;//if command starts with ./
-    if(line[0] == 'r' && line[1] == 'm')dontRead = 0;//if command is rm
-    if(line[0] == 'm' && line[1] == 'k' && line[2] == 'd' && line[3] == 'i' && line[4] == 'r')dontRead = 0;
-    //if command is mkdir
-    send(sock , line , strlen(line) , 0 );//send command to server
-    if(dontRead == 1)read(sock,line,1024);//read input from server
-    if(dontRead == 1)printf("%s\n", line);
+
+/* CLient side */
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define SIZE_OF_LINE 1024
+#define SERV_PORT 8080
+#define SIZE_OF_COMMAND_LINE 100
+
+int main(int argc, char *argv[])
+{
+
+    int client_socket = 0; // client socket used to communicate with a server
+    struct sockaddr_in servaddr;
+    /* Create a socket with the specified domain and protocol */
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket error");
+        exit(1);
     }
-    return 0; 
-} 
+    servaddr.sin_family = AF_INET;        // set the domain
+    servaddr.sin_port = htons(SERV_PORT); // set the port number
+
+    /* Convert (into a network address) and set the IP */
+    if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0)
+    {
+        printf("inet_pton error for 127.0.0.1");
+        exit(1);
+    }
+    /* Attempt to make a connection on the socket */
+    if (connect(client_socket, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("connect error");
+        exit(1);
+    }
+
+    while (1)
+    {
+        char line[SIZE_OF_LINE];
+        char output[SIZE_OF_LINE];
+        char cmd[SIZE_OF_COMMAND_LINE];
+        int stop = 0;
+        int no_output = 2;
+        read(client_socket, line, SIZE_OF_LINE);
+        while (no_output == 2)
+        {
+            printf("WJC_SHELL%s: ", line);
+
+            fgets(cmd, sizeof(cmd), stdin); //read from stdinof user the command
+            if (cmd[0] == '\n')
+            {
+                no_output = 2;
+            }
+            else if (cmd[0] == 'c' && cmd[1] == 'd')
+            {
+                no_output = 1;
+            } //if command is cd
+            else if (cmd[0] == '.' && cmd[1] == '/')
+            {
+                no_output = 1;
+            } //if command starts with ./
+            else if (cmd[0] == 'r' && cmd[1] == 'm')
+            {
+                no_output = 1;
+            } //if command is rm
+            else if (cmd[0] == 'm' && cmd[1] == 'k' && cmd[2] == 'd' && cmd[3] == 'i' && cmd[4] == 'r')
+            {
+                no_output = 1;
+            } //if command is mkdir
+            else if (cmd[0] == 's' && cmd[1] == 't' && cmd[2] == 'o' && cmd[3] == 'p')
+            {
+                no_output = 1;
+            }//if command is stop
+            else
+            {
+                no_output = 0;
+            }
+        }
+
+        memset(line, 0, sizeof(line));
+        send(client_socket, cmd, sizeof(cmd), 0);
+        if (no_output == 0)
+        {
+            read(client_socket, output, sizeof(output));
+            printf("%s", output);
+        }
+        memset(output, 0, sizeof(output));
+        memset(cmd, 0, sizeof(cmd));
+        if (stop = 1)
+        {
+            exit(1);
+        }
+    }
+
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,216 +1,371 @@
+/*  ----- Remote CLI Shell Server -----  */
+
+//      Walid Chebaro
+//      Joseph Zakher
+//      Christophe Jabbour
+
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/param.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <stdbool.h>
-#include <sys/mman.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RESET "\x1B[0m"
+#define SERV_PORT 8080
+#define BACKLOG 3
+#define SIZE_OF_COMMAND_LINE 100
+#define SIZE_OF_CWD 1024
+#define MAXLINE 1024
 
-#define PORT 4321
+/* Initializing commands of the shell */
+void cmd_help(int);
+void exec(int);
+void exec_pipes(int);
+void main_loop(int);
+void token_space(char[]);
 
-static char *myStrDup (char *str) {  //just to make a copy from a string
-    char *other = malloc (strlen (str) + 1);
-    if (other != NULL)
-        strcpy (other, str);
-    return other;
+int token_pipe(char[]);
+
+char *token[10];
+char *token_p[10];
+char cmd[SIZE_OF_COMMAND_LINE];
+char cwd[SIZE_OF_CWD];
+
+/* Implementing the help function using a struct cmd_doc of command 
+and their description, inserting them in an array cmd_table[],
+and printing them using the method cmd_help() */
+typedef struct cmd_doc
+{
+    char *cmd;
+    char *desc;
+} cmd_doc;
+
+cmd_doc cmd_table[] =
+    {{"cd", "cd - change the directory\n"},
+     {"help", "help - show the help menu\n"},
+     {"stop", "stop - stops the server\n"},
+     {NULL, "Use man command to get help for the other commands\n"}};
+
+void cmd_help(int client_socket)
+{
+    char help[strlen(cmd_table[0].desc) + strlen(cmd_table[1].desc) + strlen(cmd_table[2].desc) + strlen(cmd_table[3].desc) + 1];
+
+    strcpy(help, cmd_table[0].desc);
+    strcat(help, cmd_table[1].desc);
+    strcat(help, cmd_table[2].desc);
+    strcat(help, cmd_table[3].desc);
+
+    send(client_socket, help, sizeof(help), 0);
+    close(client_socket);
 }
-int main(){
-int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    char entry[1024] = {0}; 
-    char *hello = "Hello from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 4321 
-    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-    //                                               &opt, sizeof(opt))) 
-    // { 
-    //     perror("setsockopt"); 
-    //     exit(EXIT_FAILURE); 
-    // } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 4321 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    }     
-    while(1){
-        char cwd[PATH_MAX];
-        // printf(RED"SHELL:~#"YEL" %s:\t"RESET,getcwd(cwd, sizeof(cwd)));
-        // read(new_socket, size , sizeof());
-        char entry[1024] = {0}; 
-        send(new_socket , getcwd(cwd, sizeof(cwd)) , 1024 , 0 );
-        read( new_socket , entry, 1024);
-        entry[strlen(entry)-1] = '\0';
-        char *argv[100];
-        int argc = 0;
-        char *str = strtok (entry, " ");
-        while (str != NULL) {
-            argv[argc++] = myStrDup (str);
-            str = strtok (NULL, " ");
+
+/* In the main loop, we're printing each line number on the shell,
+and getting the user input using the cmd array. If the user entered a "|", 
+we're executing the command using pipes, and normally otherwise. */
+void main_loop(int client_socket)
+{
+    while (1)
+    {
+        getcwd(cwd, sizeof(cwd));
+        send(client_socket, getcwd(cwd, sizeof(cwd)), SIZE_OF_CWD, 0);
+        read(client_socket, cmd, SIZE_OF_COMMAND_LINE);
+        // If empty command line, do nothing
+
+        int j;
+        int p = 0;
+        for (j = 0; j < sizeof(cmd); j++)
+        {
+            if (cmd[j] == '|')
+            {
+                p = 1; // Used to set shell to pipe mode
+            }
         }
-        argv[argc] = NULL;
-        int j =0 , i = 0, count = 0,initCount , ptr = 0 , ppipe = 0;
-        //counting how many pipes do we have so that we know how many processes we need to fork
-        for(i=0;i<argc;i++)if(strcmp(argv[i],"|")==0)count++;
-        while(ppipe<argc && strcmp(argv[ppipe],"|")!=0)ppipe++;
-        
-        if(ppipe == 0){printf(RED"error\n"RESET);break;}//return break to continue
-        char * temp[100];
-        /* now we are storing each part in the temp */
-        for(i=ptr;i < ppipe;i++)temp[j++] = argv[i];
-        temp[j]=NULL;
-        i=0;j=0;ppipe++;ptr = ppipe;
-        if(ppipe == argc){
-            printf(RED"pipe cannot be the last entry\n"RESET);
-            continue;
+        if (p == 0)
+        {
+            exec(client_socket); // Normal execution
+        }
+        else
+        {
+            exec_pipes(client_socket); // Execution using pipes
+        }
+        memset(token, 0, sizeof(token));
+        memset(cmd, 0, sizeof(cmd));
+    }
+}
+
+/* If the shell is in normal mode (no piping needed),
+we're splitting the user input into tokens using " " as delimiter */
+void token_space(char line[])
+{
+    char *tok;
+    int i = 0;
+
+    tok = strtok(line, " \t\r\n\a");
+    while (tok != NULL)
+    {
+        token[i] = tok;
+        i++;
+        tok = strtok(NULL, " \t\r\n\a");
+    }
+    token[i] = NULL;
+}
+
+/* In piping mode, we're delimiting the user input by searching for "|"
+we then tokenize again using " " as delimiter to get each command separately */
+int token_pipe(char line[])
+{
+    char *tok;
+    int i = 0;
+
+    tok = strtok(line, "|");
+    while (tok != NULL)
+    {
+        token_p[i] = tok;
+        i++;
+        tok = strtok(NULL, "|");
+    }
+    token_p[i] = NULL;
+    return i - 1;
+}
+
+/* To execute the commands entered by the user, we start by calling token_space()
+to get each command on a single line separately. We check first for the presence
+of our custom functions and execute them, then we fork to use execvp and run normal 
+shell commands. If we don't create a child process to run a new program (execvp), 
+the function will execute then the shell will terminate automatically */
+void exec(int client_socket)
+{
+    token_space(cmd);
+
+    // Comparing user input (command) to our custom table of commands
+    if (strcmp(token[0], cmd_table[2].cmd) == 0)
+    {
+        // Terminate shell
+        exit(0);
+    }
+    else if (strcmp(token[0], cmd_table[0].cmd) == 0)
+    {
+        // Change directory
+        chdir(token[1]);
+    }
+    else
+    {
+        pid_t pid = fork();
+
+        if (pid == 0)
+        {
+            if (strcmp(token[0], cmd_table[1].cmd) == 0)
+            {
+                // HELP, Provides info to the user
+                cmd_help(client_socket);
+            }
+
+            else
+            {
+                dup2(client_socket, 1);
+                dup2(client_socket, 2);
+
+                // Run any other (usual) shell command
+                if (execvp(token[0], token) < 0)
+                {
+                    printf("Could not execute command\n");
+                    exit(0);
+                }
+                close(client_socket);
+            }
         }
 
-        //if there is not any pipes, excute directly
-        if(count==0){
-            //the next 2 lines are for editing the path
-            if(strcmp(temp[0],"cd")==0){chdir(temp[1]);continue;}
-            pid_t pidd=fork();
-            if(pidd==0){
-                dup2(new_socket, 1);
-                execvp(temp[0],temp);
-            }
-            else if(pidd>0)wait(NULL);
-            }
-        if(count == 1){
-            pid_t pid = fork();
-            if(pid == 0){
-                int fd[2];
-                pipe(fd);
-                pid_t pi = fork();
-                if(pi == 0){
-                    close(fd[0]);
-                    close(1);
-                    dup(fd[1]);
-                    execvp(temp[0],temp);
-                }
-                if(pi > 0){
-                    dup2(new_socket, 1);
-                    close(fd[1]);
-                    close(0);
-                    dup(fd[0]);
-                    wait(NULL);
-                    char * temp[100];
-                    int z,w=0;
-                    while(count>0){
-                        ptr = ppipe;
-                        while(ppipe<argc && strcmp(argv[ppipe++],"|")!=0);
-                        count--;
-                        }
-                    for(z=ptr; z < ppipe;z++)temp[w++] = argv[z];
-                    temp[w]=NULL;                
-                    execvp(temp[0],temp);
-
-                }
-            }
-            else if(pid > 0){
-                wait(NULL);
-            }
+        else if (pid > 0)
+        {
+            // Parent wait for child to finish execution
+            wait(NULL);
         }
-        if(count == 2){
-            pid_t p = fork();
-            if(p == 0){
-                int fd[2];
-                pipe(fd);
-                pid_t pi = fork();
-                if(pi == 0){
-                    close(fd[0]);
-                    close(1);
-                    dup(fd[1]);
-                    int pd[2];
-                    pipe(pd);
-                    pid_t pid = fork();
-                    if(pid == 0){
-                        //last child executes first command
-                        close(pd[0]);
-                        close(1);
-                        dup(pd[1]);
-                        execvp(temp[0],temp);
-                    }
-                    else if (pid > 0){
-                        //2rd last child executes 2nd command
-                        close(pd[1]);
-                        close(0);
-                        dup(pd[0]);
-                        wait(NULL);
-                        char * temp[100];
-                        int z,w=0;
-                        count--;
-                        while(count>0){
-                            ptr = ppipe;
-                            while(ppipe<argc && strcmp(argv[ppipe++],"|")!=0);
-                            count--;
-                            }
-                        for(z=ptr; z < ppipe-1;z++)temp[w++] = argv[z];
-                        temp[w]=NULL;                
-                        execvp(temp[0],temp);
-                    }
-                }
-                //main's child executes last and executes third command
-                else if (pi > 0){
-                    dup2(new_socket, 1);
-                    close(fd[1]);
-                    close(0);
-                    dup(fd[0]);
-                    wait(NULL);
-                    char * temp[100];
-                    int z,w=0;
-                    while(count>0){
-                        ptr = ppipe;
-                        while(ppipe<argc && strcmp(argv[ppipe++],"|")!=0);
-                        count--;
-                        }
-                    for(z=ptr; z < ppipe;z++)temp[w++] = argv[z];
-                    temp[w]=NULL;                
-                    execvp(temp[0],temp);
-                }
-            }
-            //main waits untill all childer execute
-            else if(p > 0) wait(NULL);
+
+        else
+        {
+            printf("failed to fork");
+            exit(1);
         }
     }
+}
+
+void exec_pipes(int client_socket)
+{
+    // Tokenizing on pipes and ditermening the number of pipes
+    int pipe_number = token_pipe(cmd);
+    int command_number = pipe_number + 1;
+    // The number of processes are equal to the number of commands
+    int pid[command_number];
+    // The total number of pipes to be made is twice the number of pipes that the user inserted
+    // in order to write and read.
+    int totalPipes = (pipe_number)*2;
+    int pipes[totalPipes];
+
+    // Initializing pipes
+    int j;
+    for (j = 0; j < totalPipes; j += 2)
+    {
+        pipe(pipes + j);
+    }
+
+    // We create processes depending on the number of commands
+    // Each process executes a command
+    int i;
+    for (i = 0; i < command_number; i++)
+    {
+        // For each command we create a new process
+        pid[i] = fork();
+
+        if (pid[i] == 0)
+        {
+
+            if (i == 0)
+            {
+
+                // Dup is used to send the output to the pipe write end (1)
+                dup2(pipes[totalPipes - 1], 1);
+                // Closing all the other pipes ends as they are not needed
+                for (int j = 0; j < totalPipes; j++)
+                {
+                    close(pipes[j]);
+                }
+
+                token_space(token_p[i]);
+
+                if (execvp(token[0], token) < 0)
+                {
+                    printf("Could not execute command at pipe 1\n");
+                    exit(0);
+                }
+
+                memset(token, 0, sizeof(token)); // Resetting the array of tokens
+            }
+            else if (i == command_number - 1)
+            {
+                // Dup is used to receive input from the pipe read end (0)
+                dup2(pipes[totalPipes - 2], 0);
+
+                // Closing the unused pipe ends
+                for (int j = 0; j < totalPipes; j++)
+                {
+                    close(pipes[j]);
+                }
+
+                token_space(token_p[i]);
+                dup2(client_socket, 1);
+                dup2(client_socket, 2);
+                if (execvp(token[0], token) < 0)
+                {
+                    printf("Could not execute command at pipe %d\n", i + 1);
+                    exit(0);
+                }
+                close(client_socket);
+
+                memset(token, 0, sizeof(token)); // Resetting the array of tokens
+            }
+            else
+            {
+                // Pipe read end will go to the stdin process
+                dup2(pipes[i + (i - 2)], 0);
+                // Process stdout will go to the pipe write end
+                dup2(pipes[i + i + 1], 1);
+                // Closing the unused pipe ends
+                for (int j = 0; j < totalPipes; j++)
+                {
+                    close(pipes[j]);
+                }
+
+                token_space(token_p[i]);
+
+                if (execvp(token[0], token) < 0)
+                {
+                    printf("Could not execute command at pipe %d\n", i);
+                    exit(0);
+                }
+
+                memset(token, 0, sizeof(token)); // Resetting the array of tokens
+            }
+        }
+        else if (pid[i] < 0)
+        {
+            printf("failed to fork\n");
+            exit(1);
+        }
+    }
+    // Closing the unused pipe ends
+    for (int j = 0; j < totalPipes; j++)
+    {
+        close(pipes[j]);
+    }
+
+    // Wait until all processes are finished
+    for (int i = 0; i < command_number; i++)
+    {
+        waitpid(pid[i], NULL, 0);
+    }
+}
+
+void *server_handler(void *socket)
+{
+    int client_socket = *(int *)socket;
+    main_loop(client_socket);
+}
+
+int main(int argc, char *argv[])
+{
+    int server_socket;                   // server socket used to listen and accept incoming connections
+    int client_socket;                   // client socket used to communicate with a connected client
+    struct sockaddr_in servaddr, client; // a struct that holds information about internet address
+    int c;
+
+    /* Create an unbound socket and return a file descriptor
+       int socket(int family, int type, int protocol)
+    */
+    // IPV4, STREAM, TCP
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket failed");
+        exit(1);
+    }
+
+    servaddr.sin_family = AF_INET;         // set the domain
+    servaddr.sin_addr.s_addr = INADDR_ANY; // set the IP without knowing the IP of the running system
+    servaddr.sin_port = htons(SERV_PORT);  // set the port number
+
+    /* Assign a local socket address (servaddr) to a socket identified by a descriptor (server_socket)
+       bind(int sockfd, struct sockaddr *my_addr, int addrlen)
+    */
+    if (bind(server_socket, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("bind failed");
+        exit(1);
+    }
+
+    if (listen(server_socket, BACKLOG) < 0)
+    {
+        perror("listen failed");
+        exit(1);
+    }
+
+    time_t ticks;
+    int client_counter = 0;
+    /* keep listening for incoming connections */
+    pthread_t thread;
+    while ((client_socket = accept(server_socket, (struct sockaddr *)&client, (socklen_t *)&c)))
+    {
+        client_counter++; // increment counter
+        /* Print current time and number of served clients */
+        ticks = time(NULL);
+        printf("Client %d connected at %s\n", client_counter, ctime(&ticks));
+
+        if (pthread_create(&thread, NULL, server_handler, &client_socket) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+    }
+
     return 0;
 }
